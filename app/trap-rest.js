cb9ee6f986dcd2d7259adf147160ea54e9719bb1
JJ Merelo <jjmerelo@gmail.com>
2014-01-21 20:22:12 +0100

    Does not fixes all, but adds some error checking

--- app/trap-rest.js
+++ app/trap-rest.js
@@ -9,31 +9,42 @@ var app=express();
 
 var conf_file = process.argv[2] || 'nodeo.json';
 
-var conf = fs.readFileSync( conf_file );
+var conf = JSON.parse(fs.readFileSync( conf_file, { encoding: 'utf8'} ));
 
+console.log(conf);
 var log = [];
 
 if ( !conf ) {
     throw "Problems with conf file";
 }
+
+log.push( conf );
 var chromosome_size = conf.l*conf.fitness.traps;
 
 var trapf = new trap.Trap( conf.fitness );
 
+console.log(trapf);
 var eo = new nodeo.Nodeo( { population_size: conf.population_size,
 			    chromosome_size: chromosome_size,
 			    fitness_func: trapf } );
 
-
+console.log(eo);
+// Definitions
 app.get('/get/best', function(req, res){
-    res.send({ chromosome : eo.population[0],
-	       fitness : eo.fitness_of[eo.population[0]]});
+	    res.send({ chromosome : eo.population[0],
+		       fitness : eo.fitness_of[eo.population[0]]});
+	    log.push( { get: process.hrtime});
 });
 
+console.log( "Almost");
+log.push( { time: process.hrtime} );
+console.log( "Starting ");
 process.nextTick( generations );
-app.listen( conf.port ) ;
 console.log( "Listening on " +conf.port );
+app.listen( conf.port ) ;
 
+
+//------------------------------------------------------
 function generations( ) {
     var generation_count = 0;
     do {
@@ -44,7 +55,10 @@ function generations( ) {
     if (generation_count > generation_run ) {
 	console.log( " Next " );
     } else {
+	log.push( {time: process.hrtime} );
+	fs.writeFileSync(conf.output, log);
 	console.log("Finished\n");
+	
     }
 }
 
